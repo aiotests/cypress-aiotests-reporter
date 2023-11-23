36b900d74d2364ade4da27b8cdadc6467a116578
aiotcms <aio-tcms@aioreports.com>
2023-11-23 12:38:06 +0530

    Correcting tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -10,8 +10,7 @@ describe('empty spec ', () => {
 
   it('NVTES-TC-5, NVTES-TC-6 : displays two todo items by default', () => {
     cy.get('.todo-list li').should('have.length', 2)
-    cy.get('.todo-list li').first().should('have.text', 'Pay electric bills')
-    cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
+    cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
     cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
   })
 })
